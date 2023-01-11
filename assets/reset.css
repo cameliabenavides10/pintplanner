402d49a156c4d79facfc6952d450f6499625d2a5
cameliabenavides10 <cameliabenavides@gmail.com>
2023-01-11 14:34:07 -0600

    added css and java

---
+++ assets/reset.css
@@ -0,0 +1,34 @@
+/* Resets the box size of every element */
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  
+  /* Gives the body element a parent height to compare against */
+  html {
+    height: 100%;
+  }
+  
+  body {
+    min-height: 100%;
+    line-height: 1;
+    font-family: sans-serif;
+  }
+  
+  h1, h2, h3, h4, h5, h6 {
+    font-size: 100%;
+  }
+  
+  /* Matches the font of special elements to the rest of the page */
+  input, select, option, optgroup, textarea, button,
+  pre, code {
+    font-size: 100%;
+    font-family: inherit;
+  }
+  
+  /* Removes default bullet points from lists */
+  ol, ul {
+    list-style: none;
+  }
+  
