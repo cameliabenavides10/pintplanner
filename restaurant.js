94a6adb99564ef5d8a86663a0e8563e012885fa7
Karen Peazzoni <119969744+kpeazzoni@users.noreply.github.com>
2023-01-13 10:34:25 -0500

    trying to push

--- restaurant.js
+++ restaurant.js
@@ -1,8 +1,8 @@
-var locationZip = "787";
-var restaurantChoiceContainer = document.getElementById('restaurants-container');
-var restaurantName = document.getElementById("restaurant-list")
-var restaurantChoice = "";
-var fetchButton = document.getElementById('fetch-button');
+let locationZip = "787";
+let restaurantChoiceContainer = document.getElementById('restaurants-container');
+let restaurantName = document.getElementById("restaurant-list")
+let restaurantChoice = "";
+let fetchButton = document.getElementById('fetch-button');
 const options = {
             method: 'GET',
             headers: {
@@ -13,28 +13,25 @@ const options = {
 
 
 function getApi() {
-    var locationsURL = 'https://restaurants-near-me-usa.p.rapidapi.com/restaurants/location/zipcode'
+  let locationsURL = 'https://restaurants-near-me-usa.p.rapidapi.com/restaurants/location/zipcode'
     locationsURL = locationsURL + "/" + locationZip + "/10"
     console.log(locationsURL);
 
   fetch(locationsURL, options)
     .then(function (response) {
-        console.log(response);
+        console.log('responses', response);
       return response.json();
     })
     .then(function (data) {
       console.log(data);
       for (var i = 0; i < data.length; i++) {
-        //Creating a h3 element and a p element
-        var restaurants = document.createElement('h3');
+      
+        let restaurants = document.createElement('h3');
 
-        //Setting the text of the h3 element and p element.
         restaurants.textContent = data[i].restaurantName;
   
 
-        //Appending the dynamically generated html to the div associated with the id="users"
-        //Append will attach the element as the bottom most child.
-        restaurantName.append(restuarants);
+        restaurantName.append(restaurants);
       }
     });
 }
@@ -53,39 +50,3 @@ getApi();
 // 	.then(response => response.json())
 // 	.then(response => console.log(response))
 // 	.catch(err => console.error(err));
-
-
-
-
-
-
-    // const options = {
-    //         method: 'GET',
-    //         headers: {
-    //             'X-RapidAPI-Key': '495e45f322msh4fca825b3c3afa3p19ec03jsnd3b134bddfbd',
-    //             'X-RapidAPI-Host': 'restaurants-near-me-usa.p.rapidapi.com'
-    //         }
-    // };
-    // var locationsURL = 'https://restaurants-near-me-usa.p.rapidapi.com/restaurants/location/zipcode'
-    //     locationsURL = locationsURL + "/" + locationZip + "/20"
-
-    // fetch(locationsURL, options)
-    //     .then(response => response.json())
-    //     // if (locationZip)
-    //     .then(response => console.log(response))
-    //     .catch(err => console.error(err));
-        
-
-
-// submitbtn.addEventListener('click', searchSubmit);
-
-
-// function restaurantList(zip) {
-//     locationZip = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&q=${city}&units=imperial`;
-//     fetch(apiUrlWeather).then((results) => {
-//         return results.json();
-//     })
-//     .then(function(data)    {
-//         displayCurrentWeather(data, city);
-//     })
-// }
