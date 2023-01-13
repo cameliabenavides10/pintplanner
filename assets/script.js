3b2c9ec593f97f50808032a3e69f7788dd159727
Reuben Schmolke <sandwichcrashers@gmail.com>
2023-01-13 11:57:07 -0800

    adding cards

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,3 @@
 var breweriesContainer = document.getElementById('breweries');
 var brewSelect = document.querySelector('#list1').value;
-var brewChoice = "aby_type="+brewSelect;
-console.log(brewChoice);
-
+var brewChoice = "&by_type="+brewSelect;
