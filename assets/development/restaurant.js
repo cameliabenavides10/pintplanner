6f0f8ee4b6aba9f89f8b82e432ec3bb4a4c4337b
Karen Peazzoni <119969744+kpeazzoni@users.noreply.github.com>
2023-01-13 14:21:57 -0500

    on click working

--- assets/development/restaurant.js
+++ assets/development/restaurant.js
@@ -22,19 +22,37 @@ function getApi() {
     .then(function (data) {
       for (var i = 0; i < data.restaurants.length; i++) {
    
-        let restaurantList = data.restaurants[i];
-        let restaurants = document.createElement('h3'); 
+        let restaurant = data.restaurants[i];
+        
+        let restaurantName = document.createElement('h3'); 
+        restaurantName.id = `restaurant${i}`
         let restraurantAddress = document.createElement('p');
        
 
-        restaurants.textContent = restaurantList.restaurantName;
-        restraurantAddress.textContent= restaurantList.address + restaurantList.zipCode;
-        restaurantChoiceContainer.append(restaurants);
+        restaurantName.textContent = restaurant.restaurantName;
+        restraurantAddress.textContent= restaurant.address + restaurant.zipCode;
+        restaurantChoiceContainer.append(restaurantName);
         restaurantChoiceContainer.append(restraurantAddress);
+
+        $(`#${restaurantName.id}`).on("click", function() {
+         console.log('restaurant name' + restaurantName + 'was clicked')
+        });
       }
     });
 }
 fetchButton.addEventListener('click', getApi);
+
+// function restaurantChoice(event) {
+//   if (!event) var event = window.event;
+//   if (event.target) targ = event.target;
+//   else if (event.srcElement) targ = event.srcElement;
+// }
+
+// a.addEventListener('click', e => {
+//   e.currentTarget; // always returns "a" element
+//   e.target; // may return "a" or "span"
+// })
+
 // getApi();
 
 // Michael's API Key: 'X-RapidAPI-Key': 'e045ca9b9amsha0dbbe5302d76a2p14b6a8jsnfe609301f0a7', 'X-RapidAPI-Host': 'restaurants-near-me-usa.p.rapidapi.com'
